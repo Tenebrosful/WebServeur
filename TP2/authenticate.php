d9b9c1e2cfd5235987ee7437bd07d78b2cc08c99
Tenebrosful <hugobernard39@gmail.com>
2020-09-10 10:50:22 +0200

    Finalisation de l'exercice 2

--- TP2/authenticate.php
+++ TP2/authenticate.php
@@ -6,17 +6,21 @@
 </head>
 <body>
 <?php
-if ($_SERVER["REQUEST_METHOD"] === 'POST') :
-    ?>
-<?php
-else:
-    ?>
-    <div>Une erreur c'est produite : une requête POST est attendu</div>
-    <div>Requête actuelle : <?php echo $_SERVER["REQUEST_METHOD"] ?></div>
-    <a href="signin.php">
-        <input type="button" value="Retourner vers la page de connection">
-    </a>
-<?php
-endif;
+
+$passwordError = "<div>ERREUR : Le nom d'utilisateur ou le mot de passe est incorrect.<a href='signin.php'><input type='button' value='Retourner sur la page de connexion'></a></div>";
+$methodError = "<div>Une erreur c'est produite : une requête POST est attendu</div><div>Requête actuelle : " . $_SERVER['REQUEST_METHOD'] . "</div><a href='signin.php'><input type='button' value='Retourner vers la page de connection'></a>";
+
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+    require "users.php";
+    if (key_exists($_POST["username"], $users) && $users[$_POST["username"]] === $_POST["password"]) {
+        session_start();
+        $_SESSION["username"] = $_POST["username"];
+        header('Location: welcome.php');
+        exit();
+    } else
+        echo $passwordError;
+}
+else
+    echo $methodError;
 ?>
 </body>
