e38a49f465513ebfcdb080f931cef817560d0b42
Tenebrosful <hugobernard39@gmail.com>
2020-09-10 10:59:43 +0200

    Gestion de la connexion

--- TP2/welcome.php
+++ TP2/welcome.php
@@ -6,9 +6,9 @@
 </head>
 <body>
 <?php
-    $notConnectError = "<div>Pour accéder à cette page, une authentification est requise : <a href='signin.php'><input type='button' value='Se connecter'></a> </div>"
-    session_start();
-    if (isset($_SESSION["username"])){
-
-    } else
-        echo $notConnectError;
+$notConnectError = "<div>Pour accéder à cette page, une authentification est requise : <a href='signin.php'><input type='button' value='Se connecter'></a> </div>";
+session_start();
+if (isset($_SESSION["username"])){
+    echo "<div>Bienvenu {$_SESSION['username']} ! Vive les Pandas Roux</div><a href='signout.php'><input type='button' value='Se déconnecter'></a>";
+} else
+    echo $notConnectError;
